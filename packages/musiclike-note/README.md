f4746728aad30df41480692e650692a17f8e57b1
azizdawhat <101396081+azizdawhat@users.noreply.github.com>
2023-12-30 18:00:22 +0100

    build(note): set-up modules build

---
+++ packages/musiclike-note/README.md
@@ -0,0 +1,11 @@
+# @musiclike/note
+
+**Note** wraps and parses a [CSS <length>](https://developer.mozilla.org/en-US/docs/Web/CSS/length) to use it as a `number`.
+
+## Authors
+
+Aziz Da <azizdawhat@gmail.com>
+
+## License
+
+[GPL-3.0-or-later]()
---
+++ packages/musiclike-note/rollup.config.js
@@ -0,0 +1,52 @@
+import { babel } from '@rollup/plugin-babel';
+
+import esLint from '@rollup/plugin-eslint';
+
+import terser from '@rollup/plugin-terser';
+
+import { dirname as dirName } from 'node:path';
+
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+/** @type {import('rollup').RollupOptions[]} */
+const options = [
+  {
+    external: [
+      new RegExp('@babel/runtime-corejs3'),
+      // ,
+    ],
+    input: [
+      './src/Note.js',
+      // ,
+    ],
+    output: [
+      {
+        dir: './dist',
+        entryFileNames: '[name].cjs',
+        exports: 'auto',
+        format: 'commonjs',
+      },
+      {
+        dir: './dist',
+        format: 'module',
+      },
+    ],
+    plugins: [
+      esLint({
+        // ,
+      }),
+      babel({
+        babelHelpers: 'runtime',
+        root: dirName(__filename),
+        rootMode: 'upward',
+      }),
+      terser({
+        keep_classnames: true,
+        keep_fnames: true,
+      }),
+    ],
+  },
+];
+
+export { options as default };
