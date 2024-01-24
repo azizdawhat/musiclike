dae149f515ab46714322097835f81b69858fb811
azizdawhat <101396081+azizdawhat@users.noreply.github.com>
2024-01-24 22:37:17 +0100

    refactor(note): refactor module object

--- packages/musiclike-note/src/Note.js
+++ packages/musiclike-note/src/Note.js
@@ -1,4 +1,4 @@
-/* eslint-disable comma-dangle, no-use-before-define */
+/* eslint-disable comma-dangle, consistent-return, no-use-before-define */
 class Note {
   /**
    * @memberof Note
@@ -63,20 +63,14 @@ class Note {
   }
 
   /**
-   * @param {Note|number|string} length
-   */ // eslint-disable-next-line consistent-return
+   * @param {NonNullable<ConstructorParameters<typeof Note>[0]>} length
+   */
   static parseLenUnit(length) {
-    if (isSymbol(length)) {
-      throw new TypeError('length is of type symbol! Not appropriate for String coercion.', {
-        cause: { value: length },
-      });
-    }
-
     const value = Number.parseFloat(length);
 
     if (Number.isFinite(value) && value) {
-      /** @type {Array<(typeof Note)['LenUNITS'][number]>} */
-      const [lenUnit] = `${length}`.match(/\D+$/) ?? [];
+      /** @type {(typeof Note)['LenUNITS'][number][]} */
+      const [lenUnit] = `${length}`.match(/\D+$/) || [];
 
       if (lenUnit) {
         return lenUnit;
@@ -96,36 +90,24 @@ class Note {
    * @param {Note|number|string} [length]
    * @param {(typeof Note)['LenUNITS'][number]} [lenUnit]
    */
-  constructor(length, lenUnit) {
-    if (isSymbol(length)) {
-      throw new TypeError('length is of type symbol! Not appropriate for String coercion.', {
-        cause: { value: length },
-      });
-    }
-
+  constructor(length, lenUnit = '') {
     this.#value = Number.parseFloat(length);
 
     if (Number.isFinite(this.#value) && this.#value) {
-      this.#lenUnit = /** @type {typeof Note} */ (this.constructor).parseLenUnit(length);
-
-      if (!this.#lenUnit) {
-        if (!isObject(lenUnit) && !isString(lenUnit) && lenUnit) {
-          if (isSymbol(lenUnit)) {
-            throw new TypeError('lenUnit is of type symbol! Not appropriate for String coercion.', {
-              cause: { value: lenUnit },
-            });
-          }
-
+      if (/\D+$/.test(length)) {
+        this.#lenUnit = /** @type {typeof Note} */ (this.constructor).parseLenUnit(length);
+      } else {
+        if (!isString(lenUnit) && typeof lenUnit !== 'object') {
           throw new TypeError(
-            `lenUnit is of type ${typeof lenUnit}! String coercion value wouldn't be a valid CSS <length> unit.`,
+            `Cannot convert a ${capitalize(typeof lenUnit)} value to a valid CSS <length> unit`,
             { cause: { value: lenUnit } }
           );
         }
 
-        this.#lenUnit = `${lenUnit ?? ''}`;
+        const str = `${lenUnit}`;
 
-        if (this.#lenUnit.startsWith('[object ')) {
-          this.#lenUnit = '';
+        if (!str.startsWith('[object ') && str !== 'null') {
+          this.#lenUnit = str;
         }
       }
     }
@@ -143,12 +125,12 @@ class Note {
     return this.#value;
   }
 }
-/* eslint-enable comma-dangle, no-use-before-define */
+/* eslint-enable comma-dangle, consistent-return, no-use-before-define */
 /**
- * @param {?} value
+ * @param {string} str
  */
-function isObject(value) {
-  return typeof value === 'object' && value !== null;
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
 }
 
 /**
@@ -158,11 +140,4 @@ function isString(value) {
   return typeof (value ?? {}).valueOf() === 'string';
 }
 
-/**
- * @param {?} value
- */
-function isSymbol(value) {
-  return typeof value === 'symbol';
-}
-// eslint-disable-next-line object-curly-newline
-export { Note as default, isObject, isString, isSymbol };
+export { Note as default, isString };
