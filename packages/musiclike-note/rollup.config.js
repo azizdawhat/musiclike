0e67e28040d977371b614009258fe1ca5ff0cc76
azizdawhat <101396081+azizdawhat@users.noreply.github.com>
2024-01-29 22:51:33 +0100

    build(note): update build modules set-up

--- packages/musiclike-note/rollup.config.js
+++ packages/musiclike-note/rollup.config.js
@@ -8,43 +8,59 @@ import { dirname as dirName } from 'node:path';
 
 import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-/** @type {import('rollup').RollupOptions[]} */
+import * as rollup from 'rollup';
+
+/** @type {rollup.ExternalOption} */
+const external = [
+  new RegExp('@babel/runtime-corejs3'),
+  // ,
+];
+/** @type {rollup.OutputOptions} */
+const output = {
+  dir: './dist',
+  format: 'module',
+};
+/** @type {rollup.InputPluginOption} */
+const plugins = [
+  esLint({
+    // ,
+  }),
+  babel({
+    babelHelpers: 'runtime',
+    root: dirName(fileURLToPath(import.meta.url)),
+    rootMode: 'upward',
+  }),
+  terser({
+    keep_classnames: true,
+    keep_fnames: true,
+  }),
+];
+/** @type {rollup.RollupOptions[]} */
 const options = [
   {
-    external: [
-      new RegExp('@babel/runtime-corejs3'),
+    external,
+    output,
+    plugins,
+    input: [
+      './src/Note.bind.js',
       // ,
     ],
+  },
+  {
+    external,
+    plugins,
     input: [
       './src/Note.js',
       // ,
     ],
     output: [
+      output,
       {
         dir: './dist',
         entryFileNames: '[name].cjs',
         exports: 'named',
         format: 'commonjs',
       },
-      {
-        dir: './dist',
-        format: 'module',
-      },
-    ],
-    plugins: [
-      esLint({
-        // ,
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        root: dirName(__filename),
-        rootMode: 'upward',
-      }),
-      terser({
-        keep_classnames: true,
-        keep_fnames: true,
-      }),
     ],
   },
 ];
